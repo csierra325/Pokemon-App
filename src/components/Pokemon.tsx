96d7e83550d9d8dda55ef268f27b896818ff5b8a
Camille <you@example.com>
2020-05-06 22:05:07 -0400

    First commit

---
+++ src/components/Pokemon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Pokemon extends React.Component{
+
+    render(){
+        return(
+            <h1>Find Your Pokemon</h1>
+        )
+    }
+}
+
+export default Pokemon;
